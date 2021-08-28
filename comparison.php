0050854b239298f51f11105ea7020cd57ffa8a9b
yeeonju <sue2024@naver.com>
2021-08-28 22:35:00 +0900

    21.08.28

---
+++ comparison.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Comparison Operators &amp;
+      Boolean data type1</h1>
+
+      <h2>1==1</h2>
+      <?php
+      var_dump(1==1);
+       ?>
+
+       <h2>1>1</h2>
+       <?php
+       var_dump(1>1);
+        ?>
+
+        <h2>1>=1</h2>
+        <?php
+        var_dump(1>=1);
+         ?>
+  </body>
+</html>
